9a7ef69092364e8120f6b161470fe6bbe17be1a5
Sam Sims <samsimss98@gmail.com>
2024-07-03 21:25:21 +0100

    feat: fastq parser

--- src/main.rs
+++ src/main.rs
@@ -3,115 +3,16 @@ use std::io::{self, BufRead, BufReader, Cursor, Read};
 
 use clap::Parser;
 use env_logger::Env;
-use log::{info, warn};
-use noodles::fasta;
+use log::info;
 use owo_colors::{OwoColorize, Stream::Stdout};
 
 use cli::Cli;
-use hashers::{calculate_final_hash, calculate_hash, HashAlgorithm};
+use hashers::calculate_final_hash;
 use parser::{auto_determine_file_type, FileType};
 
 mod cli;
 mod hashers;
 mod parser;
-const LOOKUP_TABLE: [u8; 256] = [
-    //A = 1 C = 2 G = 3 T = 4
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 0, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 0, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-];
-
-const RC_LOOKUP_TABLE: [u8; 256] = [
-    //A = 1 C = 2 G = 3 T = 4
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 4, 0, 3, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 4, 0, 3, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-];
-
-fn lookup(x: u8) -> u8 {
-    LOOKUP_TABLE[x as usize]
-}
-
-fn rc_lookup(x: u8) -> u8 {
-    RC_LOOKUP_TABLE[x as usize]
-}
-
-/// Trait to allow trimming ascii whitespace from a &[u8].
-pub trait SliceExt {
-    fn trim(&self) -> &Self;
-}
-
-impl SliceExt for [u8] {
-    /// https://stackoverflow.com/questions/31101915/how-to-implement-trim-for-vecu8
-    ///
-    /// Trim ascii whitespace (based on is_ascii_whitespace())
-    /// from the start and end of &\[u8\].
-    ///
-    /// Returns &\[u8\] with leading and trailing whitespace removed.
-    fn trim(&self) -> &[u8] {
-        let from = match self.iter().position(|x| !x.is_ascii_whitespace()) {
-            Some(i) => i,
-            None => return &self[0..0],
-        };
-        let to = self.iter().rposition(|x| !x.is_ascii_whitespace()).unwrap();
-        &self[from..=to]
-    }
-}
-
-pub fn process_fasta_reader(
-    reader: Box<dyn BufRead>,
-    output_individual: bool,
-    reverse: bool,
-    algorithm: &HashAlgorithm,
-) -> io::Result<Vec<String>> {
-    let mut reader = fasta::Reader::new(reader);
-    let mut hashes = Vec::new();
-    let mut duplicates = Vec::new();
-
-    for result in reader.records() {
-        let record = result?;
-        let record_name = std::str::from_utf8(record.name()).unwrap().to_string();
-        let seq = record.sequence().as_ref().trim();
-        let mut normal_seq = seq.iter().map(|&x| lookup(x)).collect::<Vec<u8>>();
-
-        if reverse {
-            let mut rc_seq = seq.iter().map(|&x| rc_lookup(x)).collect::<Vec<u8>>();
-            rc_seq.reverse();
-            if rc_seq < normal_seq {
-                normal_seq = rc_seq;
-            }
-        }
-        let hash = calculate_hash(algorithm, &normal_seq);
-        if output_individual {
-            println!(
-                "{}\t{}",
-                record_name.if_supports_color(Stdout, |record_name| record_name.white()),
-                hash.if_supports_color(Stdout, |hash| hash.green())
-            );
-        }
-        if hashes.contains(&hash) {
-            duplicates.push(record_name);
-        }
-        hashes.push(hash);
-    }
-    if !duplicates.is_empty() {
-        warn!("Duplicates found:");
-        for duplicate in duplicates {
-            warn!("{}", duplicate);
-        }
-    }
-    Ok(hashes)
-}
 
 fn main() -> io::Result<()> {
     let args = Cli::parse();
@@ -128,7 +29,7 @@ fn main() -> io::Result<()> {
             Box::new(BufReader::new(reader))
         } else {
             let (reader, _) = niffler::get_reader(Box::new(File::open(input_file)?)).unwrap();
-            Box::new(BufReader::new(reader))
+            Box::new(BufReader::with_capacity(1024 * 64, reader))
         };
         info!("Processing file: {}", input_file);
 
@@ -161,18 +62,24 @@ fn main() -> io::Result<()> {
         };
 
         let file_hashes = match file_type {
-            FileType::Fasta => {
-                process_fasta_reader(
-                    reader,
-                    args.individual_output,
-                    args.canonical,
-                    &args.seqhash,
-                )?
+            FileType::Fasta => parser::fasta_reader(
+                reader,
+                args.individual_output,
+                args.canonical,
+                &args.seqhash,
+            )?,
+            FileType::Fastq => parser::fastq_reader(
+                reader,
+                args.individual_output,
+                args.canonical,
+                &args.seqhash,
+            )?,
+            FileType::Unknown => {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "Unknown file type",
+                ))
             }
-            FileType::Fastq => {
-                unimplemented!("Fastq not implemented")
-            }
-            FileType::Unknown => return Err(io::Error::new(io::ErrorKind::InvalidData, "Unknown file type")),
         };
         all_hashes.extend(file_hashes);
 
--- src/parser.rs
+++ src/parser.rs
@@ -1,15 +1,178 @@
+use std::collections::HashSet;
+use std::io;
+use std::io::BufRead;
+
+use log::info;
+use log::warn;
+use noodles::fasta;
+use noodles::fastq;
+use owo_colors::OwoColorize;
+use owo_colors::Stream::Stdout;
+
+use crate::hashers::{calculate_hash, HashAlgorithm};
+
+const LOOKUP_TABLE: [u8; 256] = [
+    //A = 1 C = 2 G = 3 T = 4
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
+const RC_LOOKUP_TABLE: [u8; 256] = [
+    //A = 1 C = 2 G = 3 T = 4
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 4, 0, 3, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 4, 0, 3, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+pub fn lookup(x: u8) -> u8 {
+    LOOKUP_TABLE[x as usize]
+}
+
+pub fn rc_lookup(x: u8) -> u8 {
+    RC_LOOKUP_TABLE[x as usize]
+}
+
+/// Trait to allow trimming ascii whitespace from a &[u8].
+pub trait SliceExt {
+    fn trim(&self) -> &Self;
+}
+
+impl SliceExt for [u8] {
+    /// https://stackoverflow.com/questions/31101915/how-to-implement-trim-for-vecu8
+    ///
+    /// Trim ascii whitespace (based on is_ascii_whitespace())
+    /// from the start and end of &\[u8\].
+    ///
+    /// Returns &\[u8\] with leading and trailing whitespace removed.
+    fn trim(&self) -> &[u8] {
+        let from = match self.iter().position(|x| !x.is_ascii_whitespace()) {
+            Some(i) => i,
+            None => return &self[0..0],
+        };
+        let to = self.iter().rposition(|x| !x.is_ascii_whitespace()).unwrap();
+        &self[from..=to]
+    }
+}
+
 pub enum FileType {
     Fasta,
     Fastq,
     Unknown,
 }
 
-pub fn fasta_reader() {
-    unimplemented!()
+pub fn fasta_reader(
+    reader: Box<dyn BufRead>,
+    output_individual: bool,
+    reverse: bool,
+    algorithm: &HashAlgorithm,
+) -> io::Result<Vec<String>> {
+    let mut reader = fasta::Reader::new(reader);
+    let mut hashes = Vec::new();
+    let mut duplicates = Vec::new();
+
+    for result in reader.records() {
+        let record = result?;
+        let record_name = std::str::from_utf8(record.name()).unwrap().to_string();
+        let seq = record.sequence().as_ref().trim();
+        let mut normal_seq = Vec::with_capacity(seq.len());
+        for &x in seq {
+            normal_seq.push(lookup(x));
+        }
+
+        if reverse {
+            let mut rc_seq = Vec::with_capacity(seq.len());
+            for &x in seq {
+                rc_seq.push(rc_lookup(x));
+            }
+            rc_seq.reverse();
+            if rc_seq < normal_seq {
+                normal_seq = rc_seq;
+            }
+        }
+        let hash = calculate_hash(algorithm, &normal_seq);
+        if output_individual {
+            println!(
+                "{}\t{}",
+                record_name.if_supports_color(Stdout, |record_name| record_name.white()),
+                hash.if_supports_color(Stdout, |hash| hash.green())
+            );
+        }
+        if hashes.contains(&hash) {
+            duplicates.push(record_name);
+        }
+        hashes.push(hash);
+    }
+    if !duplicates.is_empty() {
+        warn!("Duplicates found:");
+        for duplicate in duplicates {
+            warn!("{}", duplicate);
+        }
+    }
+    Ok(hashes)
 }
 
-pub fn fastq_reader() {
-    unimplemented!()
+pub fn fastq_reader(
+    reader: Box<dyn BufRead>,
+    output_individual: bool,
+    reverse: bool,
+    algorithm: &HashAlgorithm,
+) -> io::Result<Vec<String>> {
+    let mut reader = fastq::Reader::new(reader);
+    let mut hashes = HashSet::new();
+    let mut duplicates = Vec::new();
+
+    for result in reader.records() {
+        let record = result?;
+        let record_name = std::str::from_utf8(record.name()).unwrap().to_string();
+        let seq = record.sequence().trim();
+        let mut normal_seq = Vec::with_capacity(seq.len());
+        for &x in seq {
+            normal_seq.push(lookup(x));
+        }
+
+        if reverse {
+            let mut rc_seq = Vec::with_capacity(seq.len());
+            for &x in seq {
+                rc_seq.push(rc_lookup(x));
+            }
+            rc_seq.reverse();
+            if rc_seq < normal_seq {
+                normal_seq = rc_seq;
+            }
+        }
+
+        let hash = calculate_hash(algorithm, &normal_seq);
+
+        if output_individual {
+            println!(
+                "{}\t{}",
+                record_name.if_supports_color(Stdout, |record_name| record_name.white()),
+                hash.if_supports_color(Stdout, |hash| hash.green())
+            );
+        }
+
+        if !hashes.insert(hash.clone()) {
+            duplicates.push(record_name);
+        }
+    }
+
+    if !duplicates.is_empty() {
+        warn!("Duplicates found:");
+        for duplicate in duplicates {
+            warn!("{}", duplicate);
+        }
+    }
+    Ok(hashes.into_iter().collect())
 }
 
 pub fn auto_determine_file_type(content: &str) -> FileType {

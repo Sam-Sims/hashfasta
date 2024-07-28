648d742d6bf9f88a25f07d4ca7c22a14a2e7055b
Sam Sims <samsimss98@gmail.com>
2024-07-28 20:00:48 +0100

    feat: cli help text

--- src/cli.rs
+++ src/cli.rs
@@ -3,37 +3,41 @@ use clap::Parser;
 use crate::hashers::HashAlgorithm;
 
 #[derive(Parser, Debug)]
-#[command(version, about = "Hash")]
+#[command(
+    author, version, about = "Quickly compute hashes for nucleotide sequences.", long_about = None
+)]
 pub struct Cli {
-    // read input, store multiple files in a vector
-    #[arg(value_name = "FASTA(s)", value_parser(check_input_exists))]
+    /// Input FASTA or FASTQ file(s). Can be GZ. Use "-" for stdin.
+    #[arg(
+        value_name = "FASTA(s)", required_unless_present = "help", value_parser(check_input_exists)
+    )]
     pub input: String,
 
-    // output individual hashes
+    /// Output individual hashes for each sequence (TSV).
     #[arg(short = 'i', long = "individual", action, conflicts_with = "show_duplicates")]
     pub individual_output: bool,
 
-    //reverse complement mode
+    /// Considers the canonical sequence (the lexicographically smaller of the two reverse complementary sequences).
     #[arg(short = 'c', long = "canonical", action)]
     pub canonical: bool,
 
-    // fasta
+    /// Force the input to be treated as FASTA format.
     #[arg(long = "fasta", action)]
     pub fasta: bool,
 
-    // fastq
+    /// Force the input to be treated as FASTQ format.
     #[arg(long = "fastq", action)]
     pub fastq: bool,
 
-    //display duplicates
+    /// Output duplicates sequences (TSV).
     #[arg(short = 'd', long = "duplicates", action, conflicts_with = "individual_output")]
     pub show_duplicates: bool,
 
-    // sequence hash algorithm
+    /// Specify the algorithm to use for hashing sequences.
     #[arg(long = "seqhash", default_value = "highway")]
     pub seqhash: HashAlgorithm,
 
-    // final hash algorithm
+    /// Specify the algorithm to use for calculating the final hash.
     #[arg(long = "finalhash", default_value = "md5")]
     pub finalhash: HashAlgorithm,
 }
